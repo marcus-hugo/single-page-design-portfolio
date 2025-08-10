4086992643c3006702cd23a3a7aa686bb53596b0
marcushugo <phantomboss300@yahoo.com>
2025-08-10 11:04:44 -0500

    many updates

---
+++ notes.md
@@ -0,0 +1,26 @@
+
+For a bento grid watch Kevin Powel's grid vidoe:
+https://www.youtube.com/watch?v=rg7Fvvl3taU
+https://github.com/kevin-powell/learn-grid-the-easy-way/blob/main/style.css
+
+
+
+Done:
+
+CSS reset: /* https://piccalil.li/blog/a-more-modern-css-reset/ */
+
+Added Google Fonts Plus Jakarta Sans
+
+Named css color variables
+
+styled the header and followed this accessiblilty pattern for the link with only an icon or image:
+https://a11y-101.com/development/icons-and-links
+
+Completed Bento grid layout for mobile, tablet, and desktop.
+
+mobile  and desktop styles for hero heading and text
+
+Scroll snap gallery resource:
+https://web.dev/articles/css-scroll-snap
+
+very helpful for solution. It's pretty simple and not a lot of css or js!
--- scrollSnap.js
+++ scrollSnap.js
@@ -2,16 +2,18 @@
 
 const carousel = document.querySelector('.carousel');
 const scrollContainer = carousel.querySelector('.scroll-container');
-const galleryItemSize = scrollContainer.querySelector('li').clientWidth;
+let galleryItemSize = scrollContainer.querySelector('.carousel__slide').clientWidth;
 
 carousel.querySelector('.btn-prev').addEventListener('click', scrollToPrevPage);
 carousel.querySelector('.btn-next').addEventListener('click', scrollToNextPage);
 
 
 function scrollToNextPage() {
+    galleryItemSize = scrollContainer.querySelector('.carousel__slide').clientWidth;
     scrollContainer.scrollBy(galleryItemSize, 0);
 }
 
 function scrollToPrevPage() {
+    galleryItemSize = scrollContainer.querySelector('.carousel__slide').clientWidth;
     scrollContainer.scrollBy(-galleryItemSize, 0);
 }
